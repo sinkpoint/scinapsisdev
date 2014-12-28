fe8a227ebec16562110b677a7f6bb880713aa398
sinkpoint <qixiang.chen@gmail.com>
2014-12-28 01:38:52 -0500

    - added font size to admin tiny-mce - organzed statics

--- scinapsis/src/mvp_landing/settings.py
+++ scinapsis/src/mvp_landing/settings.py
@@ -128,14 +128,20 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
     STATICFILES_DIRS = (
        os.path.join(os.path.dirname(BASE_DIR), "static"),
-       os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
     )
+
+else:
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 TINYMCE_JS_ROOT = os.path.join(STATIC_URL,'tiny_mce')
 TINYMCE_JS_URL = os.path.join(STATIC_URL, "tiny_mce/tiny_mce_src.js")
 TINYMCE_DEFAULT_CONFIG = {
-    'plugins': "table,spellchecker,paste,searchreplace",
+    'plugins': "table,spellchecker,contextmenu,paste,searchreplace,save",
     'theme': "advanced",
     'remove_trailing_brs': 'false',
+    'theme_advanced_font_sizes': "10px,12px,13px,14px,16px,18px,20px",
+    'font_size_style_values': "12px,13px,14px,16px,18px,20px",
+    'theme_advanced_buttons1':"bold,italic,underline,strikethrough,sub,sup,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,formatselect,fontselect,fontsizeselect",
+    'theme_advanced_buttons2':"bullist,numlist,outdent,indent,ltr,rtl,separator,link,unlink,anchor,image,separator,table,insertdate,inserttime,advhr,emotions,media,charmap,separator,pasteword,separator,undo,redo",
 }
 TINYMCE_SPELLCHECKER = True
