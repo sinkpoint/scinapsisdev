64771ad2d57943786faade8e5fc165aa2d60c2f2
dannyyiu <danny.yiu@gmail.com>
2014-12-31 17:43:56 -0500

    Meta tags

--- scinapsis/src/blog/models.py
+++ scinapsis/src/blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title  = models.CharField(max_length=200)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    meta = models.CharField(max_length=250)
     tags = TaggableManager ()
 
     def __unicode__(self):
